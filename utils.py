6340a1f86d6409213e6ee611fd5b1e6613c40aea
michalabramuk <272496@student.pwr.edu.pl>
2025-05-08 10:41:31 +0200

    trzeci commit

--- utils.py
+++ utils.py
@@ -1,9 +1,18 @@
-# przykladowe funkcje utils .py
+""" przykladowe funkcje utils .py"""
 def add ( a: int , b: int) -> int:
     return a + b
+"""
+opis
+"""
 def subtract ( a: int , b: int) -> int:
     return a - b
+"""
+opis
+"""
 def multiply ( a: int , b: int) -> int:
     return a * b
+"""
+opis
+"""
 def divide ( a: int , b: int) -> float :
-    return a / b
+    return a / b
