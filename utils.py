d51341a0e565ecced1ec030dbaca21f3993e12cf
michalabramuk <272496@student.pwr.edu.pl>
2025-05-08 10:48:11 +0200

    czwarty commit

--- utils.py
+++ utils.py
@@ -1,16 +1,20 @@
 ﻿"""Przykładowe funkcje w module utils.py"""
 
+
 def add(a: int, b: int) -> int:
     """Zwraca sumę a i b."""
     return a + b
+
 
 def subtract(a: int, b: int) -> int:
     """Zwraca różnicę a i b (a minus b)."""
     return a - b
 
+
 def multiply(a: int, b: int) -> int:
     """Zwraca iloczyn a i b."""
     return a * b
+
 
 def divide(a: int, b: int) -> float:
     """Zwraca iloraz a i b. Jeśli b == 0, rzuca ZeroDivisionError."""
